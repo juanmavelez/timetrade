fd92ba72162457be06f394498e8556f30c830fbe
juanmavelez <juanmanuelveleztorres@gmail.com>
2023-05-20 09:14:02 +0200

    feat: add home page endpoint

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,3 +1,14 @@
 const basePath = "https://timetrade.fly.dev"
 
-export const loginEndpoint = `${basePath}/users/sign_in.json`
+export const LOGIN_ENDPOINT = `${basePath}/users/sign_in.json`
+
+export const LOGOUT_ENDPOINT = `${basePath}/users/sign_out.json`
+
+export const CREATE_SERVICE_ENDPOINT = `${basePath}/services.json`
+
+export const HOME_PAGE_ENDPOINT = `${basePath}/services.json`
+
+export const SHOW_SERVICE_ENDPOINT = `${basePath}/services.json`
+
+export const USER_PROFILE_ENDPOINT = `${basePath}/users/show.json`
+
