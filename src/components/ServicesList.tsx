b57b85ead0f96137770db40c5419c86eaf4c69b2
juanmavelez <juanmanuelveleztorres@gmail.com>
2023-05-21 14:45:14 +0200

    improve home page

--- src/components/ServicesList.tsx
+++ src/components/ServicesList.tsx
@@ -1,9 +1,6 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
+import {Grid, Container, Typography} from '@mui/material';
 import Service, {ServiceProps} from "./Service";
 import {FC} from "react";
-import Typography from "@mui/material/Typography";
 import Link from "./Link";
 import {PROFILE_PAGE} from "../constants/urls";
 
@@ -15,13 +12,22 @@ export interface ServicesListProps {
  const ServicesList: FC<ServicesListProps> = (props) => {
     const {services_list, title} = props;
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <Typography variant="h6">
+        <Container
+            sx={{
+                display:"grid",
+                gap:"1rem",
+                paddingTop: "2rem",
+                paddingBottom: "2rem"
+        }}>
+            <Typography variant="h5" component="h2" sx={{fontWeight: "bold"}}>
                 {title}
             </Typography>
-            <Grid container spacing={2}>
+            <Grid
+                container
+                spacing={{ xs: 2, md: 3 }}
+            >
                 {services_list?.length > 1  && services_list.map((service) => {
-                    return(<Grid key={service.id} item>
+                    return(<Grid key={service.id} item xs={12} sm={3} md={4} >
                         <Service {...service}></Service>
                     </Grid>)})
                 }
@@ -31,7 +37,7 @@ export interface ServicesListProps {
                     </Link>
                     }
             </Grid>
-        </Box>
+        </Container>
     );
 }
 
