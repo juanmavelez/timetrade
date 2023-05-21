77f95cf183cca2748218dc344a20c0ba559d6787
juanmavelez <juanmanuelveleztorres@gmail.com>
2023-05-22 01:51:34 +0200

    feat/addHead

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import { Box, Typography } from "@mui/material";
+import {Box, Typography} from "@mui/material";
 import Link from "../src/components/Link";
 import {HOME_PAGE} from "../src/constants/urls";
+import Head from "next/head";
 
 const Custom500: React.FC = () => {
     return (
-        <Box sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh',
-        }}>
-            <Typography variant="h3" component="h1" sx={{fontWeight: "bold"}}>500</Typography>
-            <Typography variant="body1" component="p">Ups!Ha ocurrido un error! vuelva mas tarde</Typography>
-            <Link href={HOME_PAGE} sx={{
-                fontSize: "1rem",
+        <>
+            <Head>
+                <title>{`500 | TimeTrade`}</title>
+            </Head>
+            <Box sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
             }}>
-                Ir al home
-            </Link>
-        </Box>
+                <Typography variant="h3" component="h1" sx={{fontWeight: "bold"}}>500</Typography>
+                <Typography variant="body1" component="p">Ups!Ha ocurrido un error! vuelva mas tarde</Typography>
+                <Link href={HOME_PAGE} sx={{
+                    fontSize: "1rem",
+                }}>
+                    Ir al home
+                </Link>
+            </Box>
+        </>
     );
 };
 
--- pages/login.tsx
+++ pages/login.tsx
@@ -3,18 +3,25 @@ import {Container, Typography} from '@mui/material';
 import LoginForm from "../src/components/Login/LoginForm";
 import Link from "../src/components/Link";
 import {SIGNUP_PAGE} from "../src/constants/urls";
+import Head from "next/head";
+import * as React from "react";
 
 const Login: NextPage = () => {
     return (
-        <Container maxWidth="lg" sx={{marginTop: "3rem"}}>
-            <Typography variant="h3" component="h1" mt={2} textAlign={"center"} sx={{fontWeight: "bold"}}>Login</Typography>
-            <Typography variant="subtitle1" mt={2} textAlign={"center"}>Y continua mejorando el mundo</Typography>
-            <LoginForm></LoginForm>
+        <>
+            <Head>
+                <title>{`Login | TimeTrade`}</title>
+            </Head>
+            <Container maxWidth="lg" sx={{marginTop: "3rem"}}>
+                <Typography variant="h3" component="h1" mt={2} textAlign={"center"}
+                            sx={{fontWeight: "bold"}}>Login</Typography>
+                <Typography variant="subtitle1" mt={2} textAlign={"center"}>Y continua mejorando el mundo</Typography>
+                <LoginForm></LoginForm>
                 <Typography variant="body1" mt={2} textAlign={"center"}>
                     Eres nuevo por aqui? <Link href={SIGNUP_PAGE}>Hazte una cuenta!</Link>
                 </Typography>
-        </Container>
-    );
+            </Container>
+        </>);
 }
 
 export default Login;
