ef9d9c28469ddb6f38bbd8db3b4b44425f15099b
Juan Manuel Velez <45683136+juanmavelez@users.noreply.github.com>
2023-05-22 15:58:17 +0200

    task improvements (#44)

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -6,7 +6,9 @@ import Link from "../Link";
 import {SERVICE_PAGE} from "../../constants/urls";
 
 export const Task: FC<ITask> = (props) => {
-    const {created_at, state, service_id} = props;
+    const {created_at, state, service_id, description, contact_info, time} = props;
+
+    console.log(props)
     if (state === undefined) {
         return null
     }
@@ -14,25 +16,37 @@ export const Task: FC<ITask> = (props) => {
     const stateMutation = statePipe(state);
 
     return (
-        <Link href={`${SERVICE_PAGE}${service_id}`} sx={{padding: "1rem", border: `1px solid ${stateMutation.color}`, borderRadius: "4px", textDecoration: "none", color:"inherit"}}>
-            <Typography>
-                Solicitud
-            </Typography>
-            {created_at !== undefined &&
-                <Typography sx={{
-                    display: "grid",
-                    gridAutoFlow: "column",
-                    gridTemplateColumns: "4rem auto",
-                    marginTop: "1.5rem"
+        <Link href={`${SERVICE_PAGE}${service_id}`} sx={{
+            padding: "1rem",
+            border: `1px solid ${stateMutation.color}`,
+            borderRadius: "4px",
+            textDecoration: "none",
+            color: "inherit",
+            width: "100%",
+        }}>
+            {time !== undefined && <Typography variant="body1"> Valor: {time} Horas</Typography>}
+            {description !== undefined && <Typography mt={1}>{description}</Typography>}
+            <Box sx={{display: "grid"}}>
+                {contact_info?.firstname !== undefined && contact_info.lastname !== undefined &&
+                        <Typography mt={1}>
+                            {contact_info.firstname} {contact_info.lastname}
+                        </Typography>
+                }
+                {contact_info?.email !== undefined &&
+                    <Typography>
+                        {contact_info.email}
+                    </Typography>
+                }
+                {created_at !== undefined && <Typography sx={{
+                    display: "grid", gridAutoFlow: "column", gridTemplateColumns: "4rem auto"
                 }}>
-                    fecha: <Box sx={{fontWeight: "medium"}}>{new Date(created_at).toLocaleDateString()}</Box>
-                </Typography>
-            }
+                    Fecha: <Box sx={{fontWeight: "medium"}}>{new Date(created_at).toLocaleDateString()}</Box>
+                </Typography>}
 
-            <Typography
-                sx={{display: "grid", gridAutoFlow: "column", gridTemplateColumns: "4rem auto", marginTop: "1rem"}}>
-                Estado: <Box sx={{fontWeight: "medium"}}>{stateMutation.text}</Box>
-            </Typography>
-        </Link>
-    )
+                <Typography
+                    sx={{display: "grid", gridAutoFlow: "column", gridTemplateColumns: "4rem auto"}}>
+                    Estado: <Box sx={{fontWeight: "medium"}}>{stateMutation.text}</Box>
+                </Typography>
+            </Box>
+        </Link>)
 }
